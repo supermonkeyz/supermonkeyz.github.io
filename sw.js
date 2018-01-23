f3760ad8db21acb70e63e693d41a0be093e7ceec
zhangbo2 <zhangbo2@staff.weibo.com>
2018-01-23 18:30:10 +0800

    test sw

--- sw.js
+++ sw.js
@@ -28,3 +28,54 @@ self.addEventListener('install', (event) => {
       })
     );
   });
+
+
+  self.addEventListener('fetch', (event) => {
+    console.info('Event: Fetch');
+
+    var request = event.request;
+
+    //Tell the browser to wait for newtwork request and respond with below
+    event.respondWith(
+      //If request is already in cache, return it
+      caches.match(request).then((response) => {
+        if (response) {
+          return response;
+        }
+
+        //if request is not cached, add it to cache
+        return fetch(request).then((response) => {
+          var responseToCache = response.clone();
+          caches.open(cacheName).then((cache) => {
+              cache.put(request, responseToCache).catch((err) => {
+                console.warn(request.url + ': ' + err.message);
+              });
+            });
+
+          return response;
+        });
+      })
+    );
+  });
+
+  /*
+    ACTIVATE EVENT: triggered once after registering, also used to clean up caches.
+  */
+
+  //Adding `activate` event listener
+  self.addEventListener('activate', (event) => {
+    console.info('Event: Activate');
+
+    //Remove old and unwanted caches
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== cacheName) {     //cacheName = 'cache-v1'
+              return caches.delete(cache); //Deleting the cache
+            }
+          })
+        );
+      })
+    );
+  });
