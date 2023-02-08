d220d82fa2d1ce27904e5a8dabb0f8ea2a058780
jop <popbaby@qq.com>
2023-02-08 15:57:18 +0800

    feat: sw cache

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
-(function() {
-    // 'use strict';
+(function () {
+  // 'use strict';
 
-    // window.addEventListener('beforeinstallprompt', function(e) {
-    //   outputElement.textContent = 'beforeinstallprompt Event fired';
-    // });
+  // window.addEventListener('beforeinstallprompt', function(e) {
+  //   outputElement.textContent = 'beforeinstallprompt Event fired';
+  // });
 
-    // function registerServiceWorker() {
+  // function registerServiceWorker() {
 
-    //     if (!navigator.serviceWorker) {
-    //         return;
-    //     }
+  //     if (!navigator.serviceWorker) {
+  //         return;
+  //     }
 
-    //     navigator.serviceWorker.register('/sw.js')
-    //         .then(registration => {
-    //             console.log('Service Worker: registered');
-    //         }).catch(err => {
-    //             console.log('Service Worker: registration failed ', err);
-    //         });
-    // }
+  //     navigator.serviceWorker.register('/sw.js')
+  //         .then(registration => {
+  //             console.log('Service Worker: registered');
+  //         }).catch(err => {
+  //             console.log('Service Worker: registration failed ', err);
+  //         });
+  // }
 
-    // registerServiceWorker();
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(registration => {
-            console.log(`Service Worker registered! Scope: ${registration.scope}`);
-            })
-            .catch(err => {
-            console.log(`Service Worker registration failed: ${err}`);
-            });
-      });
-    }
+  // registerServiceWorker();
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/sw.js?t=1674142551")
+        .then((registration) => {
+          console.log(
+            `Service Worker registered! Scope: ${registration.scope}`
+          );
+        })
+        .catch((err) => {
+          console.log(`Service Worker registration failed: ${err}`);
+        });
+    });
+  }
 })();
