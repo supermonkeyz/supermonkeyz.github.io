9b79b218f29fec0aec98244a9e87809f279f601a
jop <popbaby@qq.com>
2023-02-08 17:12:01 +0800

    fix: theme switch

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ if (workbox) {
   workbox.routing.registerRoute(
     /.*\.(?:js|css)/,
     workbox.strategies.cacheFirst({
-      cacheName: "static-cache-1674142551",
+      cacheName: "static-cache-1675847468",
     })
   );
 
